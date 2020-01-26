e2bdc144546700c7cfc104774fee4f75d9dbf8c5
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-01-26 15:51:56 -0500

    Configurando una aplicación en el servidor de WebSockets con Laravel

--- config/websockets.php
+++ config/websockets.php
@@ -24,7 +24,7 @@ return [
     'apps' => [
         [
             'id' => env('PUSHER_APP_ID'),
-            'name' => env('APP_NAME'),
+            'name' => 'realtime-apps',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
