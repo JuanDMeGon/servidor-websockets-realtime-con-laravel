b6feb74d1134aadced1eb8d034b00aa777d9cb7a
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-02-13 23:58:54 -0500

    Disabling peer verificatio nto use self signed certs

--- config/websockets.php
+++ config/websockets.php
@@ -123,6 +123,8 @@ return [
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
+
+        'verify_peer' => false,
     ],
 
     /*
