0b45aba47cacae176857c32d5756ba3981671051
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-02-14 00:26:42 -0500

    Revert "Disabling peer verificatio nto use self signed certs"
    This reverts commit b6feb74d1134aadced1eb8d034b00aa777d9cb7a.

--- config/websockets.php
+++ config/websockets.php
@@ -123,8 +123,6 @@ return [
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
-
-        'verify_peer' => false,
     ],
 
     /*
