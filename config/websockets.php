7df4c6e8a8c52cdfa2d2099f8814a31c7134c9f0
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-02-14 20:16:21 -0500

    Revert "Allowing perform_dns_lookup"
    This reverts commit a09c363186d04706f1e620ca58276b298ef2f6dc.

--- config/websockets.php
+++ config/websockets.php
@@ -97,7 +97,7 @@ return [
          * Use an DNS resolver to make the requests to the statistics logger
          * default is to resolve everything to 127.0.0.1.
          */
-        'perform_dns_lookup' => true,
+        'perform_dns_lookup' => false,
     ],
 
     /*
